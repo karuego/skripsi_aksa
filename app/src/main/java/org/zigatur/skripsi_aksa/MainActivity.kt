01dccecbb8283dc982f8c3947b8a035587a6313e
karuego <66974423+karuego@users.noreply.github.com>
2024-12-08 17:54:44 +0800

    add button

--- app/src/main/java/org/zigatur/skripsi_aksa/MainActivity.kt
+++ app/src/main/java/org/zigatur/skripsi_aksa/MainActivity.kt
@@ -1,6 +1,8 @@
 package org.zigatur.skripsi_aksa
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val button1: Button = findViewById(R.id.button1)
+        val button2: Button = findViewById(R.id.button2)
+        button1.setOnClickListener {
+            Toast.makeText(this, "Tombol ditekan", Toast.LENGTH_SHORT).show()
         }
     }
 }
